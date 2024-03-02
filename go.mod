c160c472e1e4b2a9d8a8e8c28f80a12f5376c6b9
Mark Fuller <mark.fuller@influxer.com>
2024-03-01 16:33:25 -0800

    go-shopify-influxer v1.0.4

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/influxer-Engineering/go-shopify-influxer
 
 go 1.21
 
+retract (
+	v1.0.2 // testing publish issue
+	v1.0.1 // premature
+	v1.0.0 // outdated
+)
+
 require (
 	github.com/google/go-querystring v1.0.0
 	github.com/jarcoal/httpmock v1.3.0
--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 go mod tidy
 
-git commit -am "go-shopify-influxer v1.0.3"
-git tag v1.0.3
-git push origin v1.0.3
+git commit -am "go-shopify-influxer v1.0.4"
+git tag v1.0.4
+git push origin v1.0.4
 
-GOPROXY=proxy.golang.org go list -m github.com/Influxer-Engineering/go-shopify-influxer@v1.0.3
+GOPROXY=proxy.golang.org go list -m github.com/influxer-Engineering/go-shopify-influxer@v1.0.4
