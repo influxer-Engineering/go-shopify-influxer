c5ac679cb4129615c4b1e61cf774d9ac74f71853
Mark Fuller <mark.fuller@influxer.com>
2024-03-01 16:03:08 -0800

    fix publish

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ git commit -am "go-shopify-influxer v1.0.2"
 git tag v1.0.2
 git push origin v1.0.2
 
-GOPROXY=proxy.golang.org go list -m github.com/influxer-Engineering.com/go-shopify-influxer@v1.0.2
+GOPROXY=proxy.golang.org go list -m github.com/influxer-Engineering/go-shopify-influxer@v1.0.2
